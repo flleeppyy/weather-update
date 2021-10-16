2e2ca73cb4bdf90000b976f5ad6c8f84b14a7f6b
Flleeppyy <flleeppyy@gmail.com>
2021-10-16 03:22:48 -0700

    Add some shit, remove some shit, add some more stupid shit, FUCK

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,17 @@ import Discord from "discord.js"
 import type {a as WeatherInterface} from "./typings/iFUckedYOUrmOM";
 import wmo from "./wmo";
 
-const a = config.webhookLink.split("api/webhooks")[1].split("/")
-a.shift();
+const a = config.webhookLink.split("api/webhooks")[1].split("/"); a.shift();
 const webhookClient = new Discord.WebhookClient({
   id: a[0],
   token: a[1]
 });
 
-const callforecast = async () => {
-  // reference to this https://open-meteo.com/en/docs
-  console.log("Making request")
+const callforecast = async () => {  
+  const h = new Date()
+  const stirng = `${h.toLocaleDateString()} - ${h.toLocaleTimeString()}: Making request [ ]`
+  process.stdout.write(stirng);
+
   const {data: weather}: {data: WeatherInterface} = await axios.get("https://api.open-meteo.com/v1/forecast", {
     "params": {
       latitude: "38.7414",
@@ -33,22 +34,58 @@ const callforecast = async () => {
 
   const date = new Date(currentWeather.time * 1000).toLocaleDateString();
   const time = new Date(currentWeather.time * 1000).toLocaleTimeString();
+
+  const timeOfDay = (() => {
+    const hour = new Date(currentWeather.time * 1000).getHours();
+    if (hour >= 6 && hour < 12) {
+      return "morning";
+    } else if (hour >= 12 && hour < 18) {
+      return "afternoon";
+    } else if (hour >= 18 && hour < 24) {
+      return "evening";
+    } else {
+      return "night";
+    }
+  })();
+
+  const mood = (() => {
+    const temp = currentWeather.temperature;   
+    if (temp > 80) {
+      return "pleasant";
+    } else if (temp > 70) {
+      return "good";
+    } else if (temp > 60) {
+      return "happy";
+    } else if (temp > 50) {
+      return "okay";
+    } else if (temp > 40) {
+      return "meh";
+    } else if (temp > 30) {
+      return "bad";
+    } else if (temp > 20) {
+      return "terrible";
+    } else {
+      return "horrible";
+    }
+  })();
+  // capitalize the first letter of the mood
+  const moodCapitalized = mood[0].toUpperCase() + mood.slice(1);
   const embed = new Discord.MessageEmbed()
-    .setTitle("Good morning!")
+    .setTitle(`${moodCapitalized} ${timeOfDay}!`) // This wont go well lmao
     .setDescription(`Here is the current weather for ${config.location.name} as of ${date} - ${time}`)
     .addField("Temperature", currentWeather.temperature + "°F")
     .addField("Cloud Cover", weather.hourly.cloudcover[0] + "%")
     .addField("Wind Direction", currentWeather.winddirection + "°")
     .addField("Wind Speed", currentWeather.windspeed + " mph")
-    .addField("Weather Code", wmo[currentWeather.weathercode] || "None")
+    .addField("Weather Description", wmo[currentWeather.weathercode] || "None")
     .setColor("ORANGE")
     .setFooter(`Lat: ${config.location.latitude} Long: ${config.location.longitude}`)
   await webhookClient.send({
     embeds: [embed]
   })
 
-  console.log("Finished")
-
+  // rewrite the line and add a checkmark to the [ ]
+  process.stdout.write(`\r${stirng.replace("[ ]", "[✅]")}\n`);
 }
 // callforecast();
 process.on("unhandledRejection", (reason, promise) => {
@@ -58,10 +95,13 @@ process.on("unhandledRejection", (reason, promise) => {
 process.on("uncaughtException", (reason, origin) => {
   console.error("Uncaught Exception: " + reason, origin)
 })
-let job = new CronJob(
-  `0 ${config.hourOfTheDay} * * *`, callforecast
-)
-
-callforecast();
-// job.start();
-console.log("Now running.")
+if (process.env.CRON === "true") {
+  let job = new CronJob(
+    `0 ${config.hourOfTheDay} * * *`, callforecast
+  )
+  
+  job.start();
+  console.log("Now running.")
+} else {
+  callforecast();
+}
